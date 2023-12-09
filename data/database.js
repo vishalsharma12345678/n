224f84fe7bbb73ed27c5b7556cd0f5d9ae532705
vishal-9368 <vshalsha@1234@gmail.com>
2023-12-09 14:49:32 +0530

    b

--- data/database.js
+++ data/database.js
@@ -1,21 +1,21 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
-let mongodbUrl = 'mongodb://127.0.0.1:27017'
-if(process.env.MONGODB_URL){
-  mongodbUrl = process.env.MONGODB_URL; 
+let mongodbUrl = `mongodb+srv://vshalsha1234:12345adt@cluster0.u6qvost.mongodb.net/new1?retryWrites=true&w=majority`;
+if (process.env.MONGODB_URL) {
+  mongodbUrl = process.env.MONGODB_URL;
 }
 let database;
 
 async function connectToDatabase() {
   const client = await MongoClient.connect(mongodbUrl);
-  database = client.db('online-shop');
+  database = client.db("online-shop");
 }
 
 function getDb() {
   if (!database) {
-    throw new Error('You must connect first!');
+    throw new Error("You must connect first!");
   }
 
   return database;
@@ -23,5 +23,5 @@ function getDb() {
 
 module.exports = {
   connectToDatabase: connectToDatabase,
-  getDb: getDb
+  getDb: getDb,
 };
